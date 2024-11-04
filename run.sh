79eb3094c63de20009eafdd9e7ec2ab7781a604a
benjae <benja.ae@proton.me>
2024-11-04 16:46:35 -0300

    just in case

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-/home/user/bots/fangame-takes-bot/venv/bin/python3 /home/user/bots/fangame-takes-bot/main.py
-/home/user/bots/fangame-takes-bot/venv/bin/python3 /home/user/bots/fangame-takes-bot/bluesky.py
+/home/user/bots/fangame-takes-bot/venv/bin/python3 /home/user/bots/fangame-takes-bot/main.py;
+/home/user/bots/fangame-takes-bot/venv/bin/python3 /home/user/bots/fangame-takes-bot/bluesky.py;
 /home/user/bots/fangame-takes-bot/venv/bin/python3 /home/user/bots/fangame-takes-bot/twitter.py
